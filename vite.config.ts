1e34132cdb504672b224b95538bb020ab4379d60
Swastik Poojary <swastikjayaraj500@gmail.com>
2025-07-02 22:51:31 +0530

    Prepare for Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,6 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-// Detect if you're in production (for GitHub Pages)
-const isProd = process.env.NODE_ENV === "production";
-
 export default defineConfig({
-  base: isProd ? "/RIDEAtlas-EX/" : "/", // dynamic base path
-  build: {
-    outDir: "build/client", // REQUIRED for gh-pages deploy from this folder
-    emptyOutDir: true,
-  },
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 });
-
